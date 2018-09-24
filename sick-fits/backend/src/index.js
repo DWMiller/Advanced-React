d23d7f48776479dfa5da1d0c95f718fb8f85d845
David Miller <davidabc66@gmail.com>
2018-09-24 01:16:39 -0400

    done 12

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,25 @@
 // lets go!
+
+require('dotenv').config({ path: 'variables.env' });
+
+const createServer = require('./createServer');
+
+const db = require('./db');
+
+const server = createServer();
+
+// Handle cookies w/ middleware
+
+// Handle current user w/ middleware
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Running on http://localhost:${deets.port}`);
+  }
+);
