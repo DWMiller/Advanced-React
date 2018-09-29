4561125b72784ff6565eb26c2de43432b87399e1
David Miller <davidabc66@gmail.com>
2018-09-28 20:55:13 -0400

    done 19

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -44,6 +44,29 @@ export default class CreateItem extends Component {
     this.setState(() => ({ [name]: val }));
   };
 
+  uploadFile = async e => {
+    console.log('Uploading');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const response = await fetch(
+      'https://api.cloudinary.com/v1_1/dbszlrnub/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+
+    const file = await response.json();
+
+    this.setState(() => ({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    }));
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -52,8 +75,6 @@ export default class CreateItem extends Component {
             onSubmit={async e => {
               e.preventDefault();
               const response = await createItem();
-              console.log(response);
-
               Router.push({
                 pathname: '/item',
                 query: {
@@ -63,8 +84,26 @@ export default class CreateItem extends Component {
             }}
           >
             <ErrorMessage error={error} />
-
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="Upload Preview"
+                  />
+                )}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
